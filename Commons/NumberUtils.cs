5ded31f3446b7d428806e4678722ea0f8ab66348
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-10 11:05:39 +0900

    Update NumberUtils.cs

--- Commons/NumberUtils.cs
+++ Commons/NumberUtils.cs
@@ -36,10 +36,19 @@ namespace Giselle.Commons
         {
             return double.TryParse(str, out var value) ? value : fallback;
         }
+        public static double? ToDoubleNullable(string str)
+        {
+            return double.TryParse(str, out var value) ? value : new double?();
+        }
 
         public static float ToFloat(string str, float fallback = 0.0F)
         {
             return float.TryParse(str, out var value) ? value : fallback;
+        }
+
+        public static float? ToFloatNullable(string str)
+        {
+            return float.TryParse(str, out var value) ? value : new float?();
         }
 
         public static ulong ToULong(string str, ulong fallback = 0)
@@ -47,9 +56,19 @@ namespace Giselle.Commons
             return ulong.TryParse(str, out var value) ? value : fallback;
         }
 
+        public static ulong? ToULongNullable(string str)
+        {
+            return ulong.TryParse(str, out var value) ? value : new ulong?();
+        }
+
         public static long ToLong(string str, long fallback = 0)
         {
             return long.TryParse(str, out var value) ? value : fallback;
+        }
+
+        public static long? ToLongNullable(string str)
+        {
+            return long.TryParse(str, out var value) ? value : new long?();
         }
 
         public static uint ToUInt(string str, uint fallback = 0)
@@ -57,9 +76,19 @@ namespace Giselle.Commons
             return uint.TryParse(str, out var value) ? value : fallback;
         }
 
+        public static uint? ToUIntNullable(string str)
+        {
+            return uint.TryParse(str, out var value) ? value : new uint?();
+        }
+
         public static int ToInt(string str, int fallback = 0)
         {
             return int.TryParse(str, out var value) ? value : fallback;
+        }
+
+        public static int? ToIntNullable(string str)
+        {
+            return int.TryParse(str, out var value) ? value : new int?();
         }
 
         public static ushort ToUShort(string str, ushort fallback = 0)
@@ -67,9 +96,19 @@ namespace Giselle.Commons
             return ushort.TryParse(str, out var value) ? value : fallback;
         }
 
+        public static ushort? ToUShortNullable(string str)
+        {
+            return ushort.TryParse(str, out var value) ? value : new ushort?();
+        }
+
         public static short ToShort(string str, short fallback = 0)
         {
             return short.TryParse(str, out var value) ? value : fallback;
+        }
+
+        public static short? ToShortNullable(string str)
+        {
+            return short.TryParse(str, out var value) ? value : new short?();
         }
 
         public static char ToChar(string str, char fallback = '\0')
@@ -77,14 +116,29 @@ namespace Giselle.Commons
             return char.TryParse(str, out var value) ? value : fallback; 
         }
 
+        public static char? ToCharNullable(string str)
+        {
+            return char.TryParse(str, out var value) ? value : new char?();
+        }
+
         public static byte ToByte(string str, byte fallback = 0)
         {
             return byte.TryParse(str, out var value) ? value : fallback;
         }
 
+        public static byte? ToByteNullable(string str)
+        {
+            return byte.TryParse(str, out var value) ? value : new byte?();
+        }
+
         public static sbyte ToSByte(string str, sbyte fallback = 0)
         {
             return sbyte.TryParse(str, out var value) ? value : fallback;
+        }
+
+        public static sbyte? ToSByteNullable(string str)
+        {
+            return sbyte.TryParse(str, out var value) ? value : new sbyte?();
         }
 
     }
